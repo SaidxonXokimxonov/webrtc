2d789d59e406d1021f8f953565549307cba2bc3d
SaidxonXokimxonov <saidxonxokimxonov006@gmail.com>
2025-07-31 14:57:55 +0500

    ads

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: '.', // Asosiy fayllar shu papkada
   build: {
-    outDir: 'dist', // build natijalari shu papkaga chiqadi
+    outDir: 'dist',
   },
-  server: {
-    port: 3000, // localhost:3000 bo'ladi
-  },
-});
+})
